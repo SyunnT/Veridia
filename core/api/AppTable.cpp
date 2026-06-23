d17dee64f83e91d9b552f3763936f879d8dc85fe
SyunnT <register@syunn.cn>
2026-06-23 14:01:52 +0800

    更新

--- core/api/AppTable.cpp
+++ core/api/AppTable.cpp
@@ -14,11 +14,24 @@ Handle AppTable::getNewHandle()
     uint32_t index = -1;
     if (!freeList.empty()) {
         index = freeList.top();
-        
+        freeList.pop();
     }
     else {
-        
+        index = table.size();
+        table.push_back({nullptr, 1});
     }
+    table[index].obj = new Veridia::App;
+    return {index, table[index].generation};
+}
+
+void AppTable::releaseHandle(Handle handle)
+{
+    // TODO: 检查handle
+
+    delete table[handle.index].obj;
+    table[handle.index].obj = nullptr;
+    ++table[handle.index].generation;
+    freeList.push(handle.index);
 }
 
 AppTable::AppTable() = default;
